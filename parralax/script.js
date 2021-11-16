703f3bcd00b4af71cd49fbf7fa3989f14fe230b7
eKonnect <ekonn63@outlook.fr>
2021-11-16 17:43:31 +0100

    Cool

--- parralax/script.js
+++ parralax/script.js
@@ -4,7 +4,6 @@ let height = document.querySelector('.height');
 
 document.onscroll = function(){
     img.style.transform = `translateY(calc(-${scrollY}px / 4)) translateX(-25%)`;
-    // img.style.opacity = `calc(100% - ${scrollY}% / 10)`;
     h1.style.transform = `translateY(calc(-${scrollY}px / 3))`;
-    // h1.style.opacity = `calc(100% - ${scrollY}% / 10)`;
+    height.style.opacity = `calc(${scrollY}% / 3)`;
 }
