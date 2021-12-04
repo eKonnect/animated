3b401953c0701c271abe27316eeeb3d1637f3d8e
eKonnect <ekonn63@outlook.fr>
2021-12-04 19:23:40 +0100

    wow union

--- union/script.js
+++ union/script.js
@@ -1,6 +1,16 @@
 document.onscroll = function(){
-    document.querySelector('.header').style.transform = `translateY(calc(-${scrollY}px / 5))`;
+    document.querySelector('.header>p').style.transform = `translateY(calc(-${scrollY}px / 5))`;
     document.querySelector('.header>p').style.opacity = `calc(100% - ${scrollY}% / 2)`;
+    document.querySelectorAll('.height>*').forEach(function(element){
+        element.style.opacity = `calc(${scrollY}% / 2)`;
+    });
+    if(scrollY === 0){
+        document.querySelector('.height').style.opacity = '0';
+        document.querySelector('.header').style.height = 'calc(100% - 250px)';
+    } else {
+        document.querySelector('.height').style.opacity = '1';
+        document.querySelector('.header').style.height = '400px';
+    }
     if(scrollY > 1) {
         document.querySelector('.navbar').style.backgroundColor = 'white';
         document.querySelector('.navbar').style.boxShadow = '0 0 10px 0 rgba(0, 0, 0, .5)';
