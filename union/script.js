60e3eded8955460a1fa7ad0d25725ebaa9125c21
eKonnect <ekonn63@outlook.fr>
2021-12-05 19:15:41 +0100

    Parralax img header

--- union/script.js
+++ union/script.js
@@ -1,21 +1,24 @@
 document.onscroll = function(){
-    document.querySelector('.header>p').style.transform = `translateY(calc(-${scrollY}px / 5))`;
+    document.querySelector('.header>img').style.transform = `translateY(calc(-${scrollY}px / 6)) scale(calc(1 + ${scrollY} / 1000))`;
+    document.querySelector('.header>p').style.transform = `translateY(calc(-${scrollY}px / 3))`;
     document.querySelector('.header>p').style.opacity = `calc(100% - ${scrollY}% / 2)`;
     document.querySelectorAll('.height>*').forEach(function(element){
         element.style.opacity = `calc(${scrollY}% / 2)`;
     });
     if(scrollY === 0){
         document.querySelector('.height').style.opacity = '0';
-        document.querySelector('.header').style.height = 'calc(100% - 250px)';
+        // document.querySelector('.header').style.height = 'calc(100% - 250px)';
     } else {
         document.querySelector('.height').style.opacity = '1';
-        document.querySelector('.header').style.height = '400px';
+        // document.querySelector('.header').style.height = '400px';
     }
     if(scrollY > 1) {
+        document.querySelector('.navbar>.title').style.color = 'black';
         document.querySelector('.navbar').style.backgroundColor = 'white';
         document.querySelector('.navbar').style.boxShadow = '0 0 10px 0 rgba(0, 0, 0, .5)';
     } else {
-        document.querySelector('.navbar').style.backgroundColor = 'gray';
+        document.querySelector('.navbar>.title').style.color = 'white';
+        document.querySelector('.navbar').style.backgroundColor = 'rgba(0, 0, 0, .6)';
         document.querySelector('.navbar').style.boxShadow = 'none';
     }
     document.querySelector('.navbar>.part-indicator').textContent = "";
