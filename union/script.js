7750726a51eceda27caefeb270d241d38127647e
eKonnect <ekonn63@outlook.fr>
2021-12-15 21:46:28 +0100

    gallery

--- union/script.js
+++ union/script.js
@@ -1,25 +1,14 @@
 document.onscroll = function(){
     // document.querySelector('.header>img').style.transform = `translateY(calc(-${scrollY}px / 6)) scale(calc(1 + ${scrollY} / 1000))`;
-    document.querySelector('.header>img').style.transform = `translateY(calc(-${scrollY}px / 6))`;
-    document.querySelector('.header>p').style.transform = `translateY(calc(-${scrollY}px / 3))`;
+    // document.querySelector('.header>img').style.transform = `translateY(calc(-${scrollY}px / 6))`;
+    // document.querySelector('.header>p').style.transform = `translateY(calc(-${scrollY}px / 3))`;
     // document.querySelector('.header>p').style.opacity = `calc(100% - ${scrollY}% / 2)`;
-    document.querySelectorAll('.height>*').forEach(function(element){
-        element.style.opacity = `calc(${scrollY}% / 2)`;
-    });
-    if(scrollY === 0){
-        document.querySelector('.height').style.opacity = '0';
-        // document.querySelector('.header').style.height = 'calc(100% - 250px)';
-    } else {
-        document.querySelector('.height').style.opacity = '1';
-        // document.querySelector('.header').style.height = '400px';
-    }
+    // document.querySelectorAll('.height>*').forEach(function(element){
+    //     element.style.opacity = `calc(${scrollY}% / 2)`;
+    // });
     if(scrollY > 1) {
-        document.querySelector('.navbar>.title').style.color = 'black';
-        document.querySelector('.navbar').style.backgroundColor = 'white';
         document.querySelector('.navbar').style.boxShadow = '0 0 10px 0 rgba(0, 0, 0, .5)';
     } else {
-        document.querySelector('.navbar>.title').style.color = 'white';
-        document.querySelector('.navbar').style.backgroundColor = 'rgba(0, 0, 0, .6)';
         document.querySelector('.navbar').style.boxShadow = 'none';
     }
 }
